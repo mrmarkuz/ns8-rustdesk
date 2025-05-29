efc39650b0026cee89744e2ab2a5f75c1e0a2c4d
mrmarkuz <mrmarkuz666@gmail.com>
2025-05-29 19:48:12 +0200

    Readd Letsencrypt and http redirect options to UI

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -33,7 +33,34 @@
               ref="host"
             >
             </cv-text-input>
-            
+            <cv-toggle
+              value="letsEncrypt"
+              :label="$t('settings.lets_encrypt')"
+              v-model="isLetsEncryptEnabled"
+              :disabled="loading.getConfiguration || loading.configureModule"
+              class="mg-bottom"
+            >
+              <template slot="text-left">{{
+                $t("settings.disabled")
+              }}</template>
+              <template slot="text-right">{{
+                $t("settings.enabled")
+              }}</template>
+            </cv-toggle>
+            <cv-toggle
+              value="httpToHttps"
+              :label="$t('settings.http_to_https')"
+              v-model="isHttpToHttpsEnabled"
+              :disabled="loading.getConfiguration || loading.configureModule"
+              class="mg-bottom"
+            >
+              <template slot="text-left">{{
+                $t("settings.disabled")
+              }}</template>
+              <template slot="text-right">{{
+                $t("settings.enabled")
+              }}</template>
+            </cv-toggle>
               <!-- advanced options -->
             <cv-accordion ref="accordion" class="maxwidth mg-bottom">
               <cv-accordion-item :open="toggleAccordion[0]">
@@ -96,6 +123,8 @@ export default {
       },
       urlCheckInterval: null,
       host: "",
+      isLetsEncryptEnabled: false,
+      isHttpToHttpsEnabled: true,
       loading: {
         getConfiguration: false,
         configureModule: false,
@@ -104,6 +133,8 @@ export default {
         getConfiguration: "",
         configureModule: "",
         host: "",
+        lets_encrypt: "",
+        http2https: "",
       },
     };
   },
@@ -169,6 +200,9 @@ export default {
     getConfigurationCompleted(taskContext, taskResult) {
       const config = taskResult.output;
       this.host = config.host;
+      this.isLetsEncryptEnabled = config.lets_encrypt;
+      this.isHttpToHttpsEnabled = config.http2https;
+
       this.loading.getConfiguration = false;
       this.focusElement("host");
     },
@@ -235,6 +269,8 @@ export default {
           action: taskAction,
           data: {
             host: this.host,
+            lets_encrypt: this.isLetsEncryptEnabled,
+            http2https: this.isHttpToHttpsEnabled,
           },
           extra: {
             title: this.$t("settings.instance_configuration", {
